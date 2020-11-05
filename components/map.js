7ccba5b10d2f7008ff3ca83baa8cc2c686e05e4e
Rémy Hidra <remy.hidra@gmail.com>
2020-11-05 12:47:19 +0100

    Convert components to functionnal components

--- components/map.js
+++ components/map.js
@@ -1,51 +1,26 @@
 import { Box } from '@chakra-ui/core';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Cell from './cell';
 
-export default class Map extends React.Component {
-  cellSize = 10;
+export default function Map({grid, cellSize}) {
+  const [cells, setCells] = useState([]);
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      cells: [],
-    };
-  }
+  useEffect(() => {
+    const c = [];
+    grid.forEach((col, x) => col.forEach((cell, y) => cell ? c.push({x, y}) : null));
+    setCells(c);
+  }, [grid])
 
-  componentDidMount() {
-    const width = this.divRef.clientWidth;
-    const height = this.divRef.clientHeight;
-    const wGrid = Math.floor(width / this.cellSize);
-    const hGrid = Math.floor(height / this.cellSize);
-    this.grid = new Array(wGrid).fill(0).map(() => new Array(hGrid).fill(false));
-    this.grid[1][50] = true;
-    this.grid[0][50] = true;
-    this.grid[10][5] = true;
-    this.grid[9][4] = true;
-    this.grid[9][3] = true;
-    this.grid[50][50] = true;
-    this.makeCells();
-  }
-
-  makeCells() {
-    const cells = [];
-    this.grid.forEach((col, x) => col.forEach((c, y) => c ? cells.push({x, y}) : null));
-    this.setState({cells});
-  }
-
-  render() {
-    return (
-      <Box 
-          ref={element => this.divRef = element}
-          position="relative"
-          bg="linear-gradient(rgba(74, 85, 104, .3) 1px, transparent 1px), linear-gradient(90deg, rgba(74, 85, 104, .3) 1px, transparent 1px)" 
-          bgSize={`${this.cellSize}px ${this.cellSize}px`}
-          w="100%" h="100%"
-        >
-        {this.state.cells.map(({x, y}) => (
-          <Cell key={`${x}-${y}`} cellSize={this.cellSize} x={x} y={y}/>
-        ))}
-      </Box>
-    );
-  }
+  return (
+    <Box 
+        position="relative"
+        bg="linear-gradient(rgba(74, 85, 104, .3) 1px, transparent 1px), linear-gradient(90deg, rgba(74, 85, 104, .3) 1px, transparent 1px)" 
+        bgSize={`${cellSize}px ${cellSize}px`}
+        w="100%" h="100%"
+      >
+      {cells.map(({x, y}) => (
+        <Cell key={`${x}-${y}`} cellSize={cellSize} x={x} y={y}/>
+      ))}
+    </Box>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,35 @@
 import { Flex, Box, DarkMode } from '@chakra-ui/core';
+import { useEffect, useRef, useState } from 'react';
 import Console from '../components/console';
 import Controls from '../components/controls';
 import Map from '../components/map';
+import {phiStar} from '../lib/phi-star';
+
+function handleStartPathFinding() {
+  console.log('Start Phi* algorithm !');
+  phiStar();
+}
+
+const CELL_SIZE = 10;
 
 export default function Home() {
+  const [grid, setGrid] = useState([[]]);
+  const mapEl = useRef(null);
+
+  // Initialization
+  useEffect(() => {
+    const wGrid = Math.floor(mapEl.current.clientWidth / CELL_SIZE);
+    const hGrid = Math.floor(mapEl.current.clientHeight / CELL_SIZE);
+    const g = new Array(wGrid).fill(0).map(() => new Array(hGrid).fill(false));
+    g[1][50] = true;
+    g[0][50] = true;
+    g[10][5] = true;
+    g[9][4] = true;
+    g[9][3] = true;
+    g[50][50] = true;
+    setGrid(g);
+  }, []);
+
   return (
     <DarkMode>
       <Flex direction="column" w="100vw" h="100vh" p="0" bg="gray.700">
@@ -11,7 +37,7 @@ export default function Home() {
           <Box flex={.6}>
             <Controls 
                 onChange={e => console.log(e)}
-                onFindPath={() => console.log('Start Phi* algorithm !')}
+                onFindPath={() => handleStartPathFinding()}
                 onStartRobot={() => console.log('Start Robot !')}
               />
           </Box>
@@ -19,8 +45,8 @@ export default function Home() {
             <Console/>
           </Box>
         </Flex>
-        <Box flex={.8}>
-          <Map/>
+        <Box flex={.8} ref={mapEl}>
+          <Map grid={grid} cellSize={CELL_SIZE}/>
         </Box>
       </Flex>
     </DarkMode>
